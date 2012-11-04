359dc4f6d3a2899f3394313d245718947a34c043
Jim Crossley <jim@crossleys.org>
2012-11-04 09:48:47 -0500

    Expect twitter-creds as a resource rather than a file

--- src/poorpus/twitter.clj
+++ src/poorpus/twitter.clj
@@ -8,13 +8,16 @@
             [poorpus.playback :as fake])
   (:import twitter.callbacks.protocols.AsyncStreamingCallback))
 
+(def twitter-creds-format "[app-key app-secret user-token user-token-secret]")
+
 (def
-  ^{:doc "[app-key app-secret user-token user-token-secret]"
+  ^{:doc (str "Twitter creds format=" twitter-creds-format)
     :private true}
-  twitter-creds (if (.exists (io/file "/tmp/creds"))
-                  (apply oauth/make-oauth-creds
-                         (read-string (slurp "/tmp/creds")))
-                  (throw (Exception. "Missing /tmp/creds"))))
+  twitter-creds (if-let [creds (io/resource "twitter-creds")]
+                  (apply oauth/make-oauth-creds (read-string (slurp creds)))
+                  (throw (Exception. (str "Missing 'twitter-creds' resource. "
+                                          "Required format, including square brackets: "
+                                          twitter-creds-format)))))
 
 (defn ^:private handle
   "Turn a JSON chunk of async tweetness into a hash-map"
